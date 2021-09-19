5aeefd41894a72c6f85eb77f300db0cad46eab14
berdygulov <berdygulov1997@mail.ru>
2021-09-19 19:27:10 +0500

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+/* Import jQuery module (npm i jquery) */
+import $ from 'jquery';
+import Swiper from 'swiper/bundle';
+window.jQuery = $
+window.$ = $
+
+/* Function import  */
+// import * as validator from './modules/formValidator';
+console.log($('body'));
+
