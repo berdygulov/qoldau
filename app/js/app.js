2fb2977fc9a0ad9a502bd17287426ceceb3a870f
berdygulov <berdygulov1997@mail.ru>
2021-09-26 16:32:41 +0500

    select menu js

--- app/js/app.js
+++ app/js/app.js
@@ -32,6 +32,7 @@ if ($('.datepicker').length > 0) {
 /* Selectmenu initialization*/
 if ($('.selectmenu').length > 0) {
   selectmenu.openSelectMenu();
+  selectmenu.inputSelectMenu();
 }
 
 /* Input focus and blur function */
